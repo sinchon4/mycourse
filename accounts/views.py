ec5d5035c0130aa44030c522cad6c7f5e36208e2
jungeunevepark <evecomcom98@naver.com>
2022-08-28 07:40:49 +0900

    login

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User 
 from django.contrib import auth
 from django.contrib.auth import login, authenticate
-
+import json
+from django.http import JsonResponse
 
 def signup(request):
     if request.method == 'POST':
@@ -21,16 +22,18 @@ def signup(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
+        jsonObject = json.loads(request.body)
+        username = jsonObject.get("username")
+        password = jsonObject.get("password")
         user = auth.authenticate(request, username=username, password=password)
         
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            msg= ""
         else:
-            return render(request, 'badlogin.html', {'error' : 'username or password is incorrect.'})
+            msg = "❗아이디와 비밀번호를 다시 확인해주세요"
+        data = {'msg': msg}
+        return JsonResponse(data)
     else:
         return render(request, 'login.html')
 
